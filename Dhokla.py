c1b6c89f5db8a54807e33cfa5803c42c5ca77e12
dakshuSarangdevot <sarangdevotarjunsingh9@gmail.com>
2026-03-24 20:32:36 +0530

    Update Dhokla.py

--- Dhokla.py
+++ Dhokla.py
@@ -225,4 +225,5 @@ if __name__ == "__main__":
     telegram_app.initialize()
     telegram_app.bot.set_webhook(f"{WEBHOOK_URL}/{BOT_TOKEN}")
 
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
