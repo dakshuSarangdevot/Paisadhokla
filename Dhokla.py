eb321d7a3322a1fd3075031b51ca8fa4ccc218d4
dakshuSarangdevot <sarangdevotarjunsingh9@gmail.com>
2026-03-25 19:23:14 +0530

    Update Dhokla.py

--- Dhokla.py
+++ Dhokla.py
@@ -677,11 +677,14 @@ def webhook():
         telegram_app.bot
     )
 
-    import asyncio
-    asyncio.get_event_loop().create_task(
-    telegram_app.process_update(update)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    loop.run_until_complete(
+        telegram_app.process_update(update)
     )
-    
+
+    loop.close()
 
     return "OK"
 
